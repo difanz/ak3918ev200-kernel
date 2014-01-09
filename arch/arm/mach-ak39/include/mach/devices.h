c9094665e93fbd179478b7b485f8ec05ac89c1db
Cao Lianming <no-reply@anyka.com>
2014-01-09 12:00:00 +0000

    spi: anyka: add platform device
    devices.c/devices.h: ak39_spi1_device (ak-spi) with its MMIO/IRQ resources.

--- arch/arm/mach-ak39/include/mach/devices.h
+++ arch/arm/mach-ak39/include/mach/devices.h
@@ -14,5 +14,7 @@ extern struct platform_device ak39_i2c_device;
 extern struct platform_device ak39_usb_otg_hcd_device;
 extern struct platform_device ak39_mac_device;
 
+extern struct platform_device ak39_spi1_device;
+
 #endif /* endif _MACH_DEVICES_H */
 
