a3ea97670dd2647ad1c6fd83208a517d73971441
Cao Lianming <no-reply@anyka.com>
2014-01-09 12:00:00 +0000

    net: anyka: add MAC platform device
    devices.c/devices.h: ak39_mac_device (ak_ethernet) with its MMIO/IRQ resources.

--- arch/arm/mach-ak39/include/mach/devices.h
+++ arch/arm/mach-ak39/include/mach/devices.h
@@ -13,6 +13,7 @@ extern struct platform_device ak39_i2c_device;
 
 extern struct platform_device ak39_usb_udc_device;
 extern struct platform_device ak39_usb_otg_hcd_device;
+extern struct platform_device ak39_mac_device;
 
 
 #endif /* endif _MACH_DEVICES_H */
