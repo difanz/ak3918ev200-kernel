a883ee3c1fd06e64bc669c4142f7ca58630ee225
Cao Lianming <no-reply@anyka.com>
2014-01-09 12:00:00 +0000

    uio: anyka: add video codec platform device
    devices.c/devices.h: ak39_mmx_device (uio_vcodec), a UIO device exposing the H.264 video codec engine's MMIO to userspace.

--- arch/arm/mach-ak39/include/mach/devices.h
+++ arch/arm/mach-ak39/include/mach/devices.h
@@ -17,6 +17,7 @@ extern struct platform_device ak39_mac_device;
 extern struct platform_device ak39_spi1_device;
 extern struct platform_device ak39_camera_interface;
 extern struct platform_device ak39_pcm_device;
+extern struct platform_device ak39_mmx_device;
 
 extern struct platform_device ak39_led_pdev;
 extern struct platform_device ak39_gpio_keys_device;
