6c02117f930f6c7f79c333c5ecbc343640923c06
Cao Lianming <no-reply@anyka.com>
2014-01-09 12:00:00 +0000

    mmc: anyka: add AK39 and SDIO platform devices
    devices.c/devices.h: ak39_mmc_device and ak39_sdio_device with their MMIO/IRQ resources.

--- arch/arm/mach-ak39/include/mach/devices.h
+++ arch/arm/mach-ak39/include/mach/devices.h
@@ -6,5 +6,8 @@ extern struct platform_device ak39_uart0_device;
 extern struct platform_device ak39_uart1_device;
 extern struct platform_device ak39_gpio_uart_device;
 
+extern struct platform_device ak39_mmc_device;
+extern struct platform_device ak39_sdio_device;
+
 #endif /* endif _MACH_DEVICES_H */
 
