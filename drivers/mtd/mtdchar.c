9d3fa37724a1c15872b71faaf40bb8202bc5419a
Alex Zhang <alex@osqdu.org>
2026-08-04 21:19:01 +0000

    mtd: anyka: fix anyka_types.h include path for mach-anycloud

--- drivers/mtd/mtdchar.c
+++ drivers/mtd/mtdchar.c
@@ -36,7 +36,7 @@
 #include <linux/mtd/mtd.h>
 #include <linux/mtd/partitions.h>
 #include <linux/mtd/map.h>
-#include <plat-anyka/anyka_types.h>
+#include <mach/anyka_types.h>
 #include <asm/uaccess.h>
 
 #include "mtdcore.h"
